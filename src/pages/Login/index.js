8c8d4390407eb1c874914e2c664a73589829c769
izassb <izabelly.00000830234@unicap.br>
2022-09-29 10:23:13 -0300

    continued css login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Login() {
   return (
@@ -13,33 +14,33 @@ function Login() {
           <h1>CoFrinn</h1>
           <h5>A melhor forma de gerenciar suas despesas financeiras</h5>
         </div>
-        <Form id="input">
+        <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Control type="email" placeholder="Digite seu email" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Control type="password" placeholder="Digite sua senha" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Lembre-se de mim" />
-          </Form.Group>
+          <div className="description">
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Lembre-se de mim" />
+            </Form.Group>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">Esqueceu sua senha?</Nav.Link>
+            </Nav.Item>
+          </div>
         </Form>
-        <ButtonGroup>
-          <Button variant="primary" size="lg" background-color="blue"> Entrar </Button>
-          <Button variant="primary" size="lg"> Entrar com Google </Button>
-        </ButtonGroup>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Esqueceu sua senha?</Nav.Link>
-        </Nav.Item>
-        <h5>
-          Ainda não tem uma conta?
-          <Nav.Link eventKey="link-1">
-            {' '}
-            Clique aqui
-            {' '}
-          </Nav.Link>
-          para cadastrar-se
-        </h5>
+        <div className="buttons">
+          <ButtonGroup>
+            <Button variant="second" size="lg"> Entrar </Button>
+            <Button variant="primary" size="lg"> Entrar com Google </Button>
+          </ButtonGroup>
+        </div>
+        <div className="description2">
+          <h5>
+            Ainda não tem uma conta? Clique aqui para cadastrar-se
+          </h5>
+        </div>
       </div>
     </div>
   );
