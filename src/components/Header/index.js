b1317d87c114c4833e04dbc6b1dd378b945dfb68
Quezia Aquino <q.de.oliveira.aquino@avanade.com>
2022-10-11 12:30:07 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from 'react';
+import * as C from './styles'
+const Header = () => {
+  return (
+    <C.Container>
+        <C.Header>
+            <C.Title>CoFrinn</C.Title>
+        </C.Header>
+    </C.Container>
+  )
+};
+export default Header;
---
+++ src/styles/global.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import { createGlobalStyle } from 'styled-components';
+
+const Global = createGlobalStyle`
+
+*{
+    margin:0;
+    padding: 0;
+}
+body {
+
+    font-family: 'Roboto', sans-serif;
+    background-color:  #f2f2f2;
+}
+`;
+
+export default Global;
