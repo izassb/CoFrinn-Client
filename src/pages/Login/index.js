7a39779fe2dba6dbf8c544064306e80a8c9b496d
izassb <izabelly.00000830234@unicap.br>
2022-10-12 17:07:32 -0300

    user validation

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function Login() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <div className="content">
       <div className="container">
@@ -13,12 +26,27 @@ function Login() {
           <h1 className="title">CoFrinn</h1>
           <h5 className="subtitle">A melhor forma de gerenciar suas despesas financeiras</h5>
         </div>
-        <Form>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Digite seu email" />
+            <InputGroup id="inputGroupPrepend" hasValidation>
+              <Form.Control
+                type="email"
+                placeholder="Digite seu email"
+                aria-describedby="inputGroupPrepend"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Email inválido.
+              </Form.Control.Feedback>
+            </InputGroup>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Digite sua senha" />
+            <InputGroup id="inputGroupPrepend" hasValidation>
+              <Form.Control type="password" placeholder="Digite sua senha" aria-describedby="inputGroupPrepend" required />
+              <Form.Control.Feedback type="invalid">
+                Senha inválida.
+              </Form.Control.Feedback>
+            </InputGroup>
           </Form.Group>
           <div className="description">
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -28,11 +56,11 @@ function Login() {
               <Nav.Link eventKey="link-1">Esqueceu sua senha?</Nav.Link>
             </Nav.Item>
           </div>
+          <div className="buttons">
+            <Button variant="second" size="lg" type="submit"> Entrar </Button>
+            <Button variant="primary" size="lg"> Entrar com Google </Button>
+          </div>
         </Form>
-        <div className="buttons">
-          <Button variant="second" size="lg"> Entrar </Button>
-          <Button variant="primary" size="lg"> Entrar com Google </Button>
-        </div>
         <div className="description2">
           <h5 className="text-description2">
             Ainda não tem uma conta?
