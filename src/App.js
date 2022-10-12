677e65c8876659c2b35d728425186fe13dcdb7bb
Quezia Aquino <q.de.oliveira.aquino@avanade.com>
2022-10-12 12:57:16 -0300

    criando os cards com valores totais

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 /* eslint-disable */
 import React from 'react';
 import Header from './components/Header';
+import Resume from './components/Resume';
 import GlobalStyle from './styles/global';
-
-export default function App() {
+// import Resume from './components/Resume'
+const App = () => {
   return (
     <>
     <Header />
-
+    <Resume />
     <GlobalStyle />
     </>
   );
-}
+};
+export default App;
